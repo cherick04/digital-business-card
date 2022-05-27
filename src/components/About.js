64fd05ed8fa73b826b0ba5b71702ca8a2cd82134
Erick C <cherick04@hotmail.com>
2022-05-27 16:29:29 -0500

    Styled page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 export default function About() {
   return (
-    <div>
+    <div className="about">
       <h3 className="title">About</h3>
       <p className="content">
         Electrical Engineer by training and programmer by passion, focused on
         Blockchain development. The idea of creating an integrated and
-        trasparent world fascinates me and I'm ready to collaborate in the
-        space.
+        trasparent world fascinates me and I'm ready to collaborate in this new
+        and powerful space.
       </p>
     </div>
   )
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,6 +1,6 @@
 export default function Interests() {
   return (
-    <div>
+    <div className="interests">
       <h3 className="title">Interests</h3>
       <p className="content">
         My interests outside of the programming world are many. I mainly like to
